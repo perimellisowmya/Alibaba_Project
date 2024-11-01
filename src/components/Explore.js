0b17a4c327c45646a6d1f2201c4b51bb078059d5
perimellisowmya <perimellisowmya@gmail.com>
2024-11-01 11:33:53 +0530

    after responsive sample done2

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -6,12 +6,12 @@ import IconTextCarousel from "./Carousel";
 function Explore() {
   return (
     <div>
-      <div className="px-4 sm:px-6 md:px-8 lg:px-16 mt-20 md:mt-40 grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto">
+      <div className="px-4 sm:px-6 md:px-8 lg:px-16 mt-20 md:mt-40 grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto text-center md:text-left justify-center">
         <h1 className="text-3xl sm:text-4xl md:text-5xl  mx-auto md:mx-0">
           Explore millions of offerings tailored to your business needs
         </h1>
 
-        <div className="grid grid-cols-2 grid-rows-2 md:ml-14 lg:ml-56 gap-4 mt-8 md:mt-1 mx-auto">
+        <div className="grid grid-cols-2 grid-rows-2 md:ml-14 lg:ml-56 gap-4 mt-8 md:mt-1 mx-auto ">
           <div className="grid grid-rows-2   gap-4">
             <div className="flex items-center">
               <div className="w-1 h-10 md:h-16 bg-gray-400 mr-4"></div>
